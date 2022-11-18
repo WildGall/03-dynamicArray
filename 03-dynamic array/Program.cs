6302bfcd8d926227faabc3d3334ea5a917a3b100
WildGall <107027191+WildGall@users.noreply.github.com>
2022-11-18 09:14:10 +0300

    Добавьте файлы проекта.

---
+++ 03-dynamic array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_dynamic_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Sum = "sum";
+            const string Exit = "exit";
+            bool isWorkProgram = true;            
+            List<int> userNumbers = new List<int>();
+
+            while (isWorkProgram)
+            {
+                Console.Write("Введите число или команду: ");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {                   
+                   default:
+                        userNumbers.Add(int.Parse(userInput));
+                        break;
+                    case Sum:
+                        int sumNumbers = 0;
+
+                        for (int i = 0; i < userNumbers.Count; i++)
+                        {
+                            sumNumbers += userNumbers[i];
+                        }
+                        Console.WriteLine(sumNumbers);
+                        break;
+                    case Exit:
+                        isWorkProgram = false;
+                        break;
+                }                
+            }
+        }
+    }
+}
